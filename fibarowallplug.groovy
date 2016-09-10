ee885b545021c3743104f89a1ab56fbbd1620a7b
cscheiene <cscheiene@gmail.com>
2016-09-10 20:08:30 +0200

    Update fibarowallplug.groovy

--- fibarowallplug.groovy
+++ fibarowallplug.groovy
@@ -180,8 +180,8 @@ def configure() {
 	delayBetween([
     
         zwave.configurationV1.configurationSet(parameterNumber: 40, size: 1, scaledConfigurationValue: 1).format(),   // Immediate power report. Available settings: 1 - 100 (%).
-        zwave.configurationV1.configurationSet(parameterNumber: 42, size: 1, scaledConfigurationValue: 1).format(), 	// Standard power load reporting. Available settings: 1 - 100 (%).
-        zwave.configurationV1.configurationSet(parameterNumber: 43, size: 1, scaledConfigurationValue: 1).format(), 	// Power reporting frequency. Available settings: 1 - 254 (s)
+        zwave.configurationV1.configurationSet(parameterNumber: 42, size: 1, scaledConfigurationValue: 1).format(), 	// Standard power reporting. Available settings: 1 - 100 (%).
+        zwave.configurationV1.configurationSet(parameterNumber: 43, size: 1, scaledConfigurationValue: 3).format(), 	// Standard power reporting frequency. Available settings: 1 - 254 (s)
         zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:[zwaveHubNodeId]).format(),
         zwave.associationV1.associationSet(groupingIdentifier:2, nodeId:[zwaveHubNodeId]).format(),
         zwave.associationV1.associationSet(groupingIdentifier:3, nodeId:[zwaveHubNodeId]).format(),
